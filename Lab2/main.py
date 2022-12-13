b2e460605084f20af0a1923387e54c8f2bb0e738
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2022-12-14 00:20:27 +0500

    update main

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,5 @@
 import json
+from my_class import *
 
 
 def load_to_json(filename):
@@ -8,6 +9,15 @@ def load_to_json(filename):
 
 def write_to_json(data, filename):
     with open(filename, 'w') as file:
-        json.dump(data, file)
+        json.dump(data, file, indent=4)
 
 
+data = {"Persons": []}
+
+for i in range(10):
+    data['Persons'].append(Person().__dict__)
+
+write_to_json(data, 'data.json')
+read = load_to_json('data.json')
+print('Имя второго человека и возраст третьего')
+print(read['Persons'][1]['name'], read['Persons'][2]['age'])
--- Lab2/my_class.py
+++ Lab2/my_class.py
@@ -3,5 +3,5 @@ from random import randint, choice
 
 class Person:
     def __init__(self):
-        self.name = choice(['Jim', 'Tom', 'Nick', 'Ann'])
+        self.name = choice(['Jim', 'Tom', 'Nick', 'Ann', 'Jack', 'George', 'Oliver'])
         self.age = randint(1, 80)
