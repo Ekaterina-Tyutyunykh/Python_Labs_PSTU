42897fce0b9b3c569c6bf79ef2f2969738b3641f
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2022-12-22 11:57:53 +0500

    add factorial

--- Lab3.py
+++ Lab3.py
@@ -1,7 +1,16 @@
+
 from pyDatalog import pyDatalog
 
-sum_of_numbers = ((0+999999)*1000000)/2 #сумма ряда считается по парам
+sum_of_numbers = ((0 + 999999) * 1000000) / 2  # сумма ряда считается по парам
 
-average_of_numbers = sum_of_numbers/1000000
+average_of_numbers = sum_of_numbers / 1000000
 
-median_of_numbers = 100/2                 #так как диапазон 1-100
+median_of_numbers = 100 / 2  # так как диапазон 1-100
+
+pyDatalog.create_terms('Sum_of_numbers, Average_of_numbers,Median_of_numbers,Prod_series_of_numbers_sqrt')
+
+pyDatalog.create_terms('Factorial, N')
+Factorial[N] = N * Factorial[N - 1] # произведене 1-100 = 100!
+Factorial[1] = 1
+
+print((Sum_of_numbers==sum_of_numbers)&(Average_of_numbers==Sum_of_numbers/1000000)&(Median_of_numbers==median_of_numbers)&(Prod_series_of_numbers_sqrt==Factorial[100]))
